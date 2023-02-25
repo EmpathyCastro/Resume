98b22aa89a2cd79eb7e96b1c1c69812e4e04cc20
Empathy Castro <empathy.castro@colorado.edu>
2023-02-25 13:14:49 -0700

    Portfolio base

--- web/routes/base_routes.py
+++ web/routes/base_routes.py
@@ -4,4 +4,14 @@ import flask
 
 @app.route("/")
 def home():
-    return flask.render_template("home.html", no_container=True)
+    return flask.redirect(flask.url_for("resume"))
+
+
+@app.route("/resume")
+def resume():
+    return flask.render_template("resume.html", no_container=True, portfolio_btn=True)
+
+
+@app.route("/portfolio")
+def portfolio():
+    return flask.render_template("portfolio.html", no_container=True, resume_btn=True)
