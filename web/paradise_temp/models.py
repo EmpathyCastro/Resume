16a40c25ce1678454810fd4e92d2f86ddf4cc90c
Empathy Castro-Chilton <empathy.castro@colorado.edu>
2023-09-25 13:48:52 -0700

    paradise inventory page start

--- web/paradise_temp/models.py
+++ web/paradise_temp/models.py
@@ -1,4 +1,4 @@
-from web.paradise_temp.db import BaseDocument, DB, DataType, ReferenceType
+from web.paradise_temp.db import BaseDocument, DB, DataType, ReferenceType, DictType, ListType
 from datetime import datetime
 
 
@@ -28,3 +28,54 @@ class Product(BaseDocument):
 
     def get_pretty_price(self):
         return f"${self['price']:.2f}"
+
+
+class ParadiseImage(BaseDocument):
+    collection = DB.inventory_images
+    fields = {
+        "extension": DataType(str, nullable=False),
+        "image": DataType(bytes, nullable=False)
+    }
+
+
+class InventoryItem(BaseDocument):
+    collection = DB.inventory_items
+    fields = {
+        "name": DataType(str, nullable=False),
+        "amount": DataType(int, nullable=False),
+        "image_id": ReferenceType(nullable=True)
+    }
+
+
+class InventoryBox(BaseDocument):
+    collection = DB.inventory_boxes
+    fields = {
+        "name": DataType(str, nullable=False),
+        "items": ListType(DictType({
+            "item_id": ReferenceType(nullable=False),
+            "amount": DataType(int, nullable=False)
+        }), nullable=False),
+        "image_id": ReferenceType(nullable=True)
+    }
+
+    def get_item_ids(self):
+        return [item["item_id"] for item in self["items"]]
+
+    def get_amount(self, items):
+        item_ids = self.get_item_ids()
+        if items:
+            items = [item for item in items if item["_id"] in item_ids]
+        else:
+            items = list(InventoryItem.find({"_id": {"$in": self["items"]}}))
+
+        max_amount = -1
+        for item in items:
+            item_box_amount = self.get_item_box_amount(item, item_ids)
+            if max_amount == -1 or item_box_amount < max_amount:
+                max_amount = item_box_amount
+
+        return max(max_amount, 0)
+
+    def get_item_box_amount(self, item, item_ids):
+        item_index = item_ids.index(item["_id"])
+        return item["amount"] // self["items"][item_index]["amount"]
--- web/paradise_temp/paradise_routes.py
+++ web/paradise_temp/paradise_routes.py
@@ -1,18 +1,36 @@
 import flask
 from web import app
-from web.paradise_temp.models import Sale, Product
+from web.paradise_temp.models import Sale, Product, InventoryItem, InventoryBox
 from web.paradise_temp.db import get_obj_id
 from datetime import datetime
 
 
 @app.route("/paradise")
-def paradise():
+def paradise_inventory():
+    inventory_boxes = list(InventoryBox.find())
+    inventory_items = list(InventoryItem.find())
+    return flask.render_template("paradise_temp/inventory.html", inventory_boxes=inventory_boxes,
+                                 inventory_items=inventory_items)
+
+
+@app.route("/paradise/modify_inventory", methods=["POST"])
+def modify_inventory():
+    item_id = flask.request.json["item_id"]
+    amount_change = flask.request.json["amount_change"]
+    item = InventoryItem.find({"_id": get_obj_id(item_id)}, one=True)
+    item["amount"] += amount_change
+    item.push()
+    return flask.jsonify({"amount": item["amount"]})
+
+
+@app.route("/paradise_pos")
+def paradise_pos():
     products = list(Product.find())
     total_earned = sum([product.get_total_earned() for product in products])
-    return flask.render_template("paradise_temp/paradise.html", products=products, total_earned=total_earned)
+    return flask.render_template("paradise_temp/paradise_pos.html", products=products, total_earned=total_earned)
 
 
-@app.route("/paradise/modify_sale", methods=["POST"])
+@app.route("/paradise_pos/modify_sale", methods=["POST"])
 def modify_sale():
     product_name = flask.request.json["product_name"]
     product_id = flask.request.json["product_id"]
